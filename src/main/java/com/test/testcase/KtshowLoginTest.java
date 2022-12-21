32c339d9e923214776d4a04f636419c367cddcb0
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2022-12-21 17:53:42 +0900

    Update KtshowLoginTest.java

--- src/main/java/com/test/testcase/KtshowLoginTest.java
+++ src/main/java/com/test/testcase/KtshowLoginTest.java
@@ -33,7 +33,7 @@ public class KtshowLoginTest extends BaseSetting {
 		driver.findElement(By.xpath(
 				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.ImageView"))
 				.click();
-		//팝업 존재 확인
+		// 팝업 존재 확인
 		if (driver.findElements(By.xpath(
 				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.app.Dialog/android.view.View/android.view.View[2]/android.widget.Button[2]"))
 				.size() > 0) {
@@ -58,7 +58,7 @@ public class KtshowLoginTest extends BaseSetting {
 		driver.findElement(By.xpath(
 				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[2]/android.view.View[2]/android.widget.EditText"))
 				.sendKeys("**********");
-        //로그인버튼
+		// 로그인버튼
 		driver.findElement(By.xpath(
 				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View[3]/android.view.View[1]/android.widget.Button"))
 				.click();
@@ -66,8 +66,7 @@ public class KtshowLoginTest extends BaseSetting {
 		driver.findElement(By.id("com.ktshow.cs:id/btn_confirm")).click();
 		//
 
-
-		//driver.findElement(By.id("com.ktshow.cs:id/btn_confirm")).click();
+		// driver.findElement(By.id("com.ktshow.cs:id/btn_confirm")).click();
 
 		driver.findElement(By.id("com.ktshow.cs:id/tv_confirm")).click();
 
