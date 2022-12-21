4fd25dfe300103fe3858bd6cd948a41144820e90
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2022-12-21 17:53:53 +0900

    Update GigagenieLoginTest.java

--- src/main/java/com/test/testcase/GigagenieLoginTest.java
+++ src/main/java/com/test/testcase/GigagenieLoginTest.java
@@ -14,24 +14,16 @@ public class GigagenieLoginTest extends BaseSetting {
 		// 첫시작 스크롤 가이드화면
 		Thread.sleep(6000);
 
-		//접근권한 설정하기가 나오는경우(S10에 나옴)
-		if (driver.findElements(By.id("com.kt.gigagenie.mobile:id/btnBottom"))
-				.size() > 0) {
+		// 접근권한 설정하기가 나오는경우(S10에 나옴)
+		if (driver.findElements(By.id("com.kt.gigagenie.mobile:id/btnBottom")).size() > 0) {
 			System.out.println("FOUND!");
-			driver.findElement(By.id(
-					"com.kt.gigagenie.mobile:id/btnBottom"))
-					.click();
+			driver.findElement(By.id("com.kt.gigagenie.mobile:id/btnBottom")).click();
 
-			driver.findElement(By.id(
-					"com.android.permissioncontroller:id/permission_allow_button"))
-					.click();
-			driver.findElement(By.id(
-					"com.android.permissioncontroller:id/permission_allow_button"))
-					.click();
+			driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_button")).click();
+			driver.findElement(By.id("com.android.permissioncontroller:id/permission_allow_button")).click();
 		} else {
 			System.out.println("NOT FOUND!");
 		}
-
 
 		// 1. 로그인
 		// 로그인 버튼 클릭(케이티아이디)
