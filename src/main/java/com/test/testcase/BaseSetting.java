8caf03ddf8ccb53093fc9d753b8ef84a6443f1e5
Jeongdong <59277907+poplar0713@users.noreply.github.com>
2022-12-21 17:53:58 +0900

    Update BaseSetting.java

--- src/main/java/com/test/testcase/BaseSetting.java
+++ src/main/java/com/test/testcase/BaseSetting.java
@@ -12,42 +12,41 @@ import org.testng.annotations.Parameters;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
-public class BaseSetting  {
-    public        AndroidDriver<MobileElement> driver;
+public class BaseSetting {
+	public AndroidDriver<MobileElement> driver;
 
-    @BeforeTest(alwaysRun = true)
-    @Parameters({"deviceName", "udid", "systemPort", "appPackage", "appActivity"})
-    public void setup(String deviceName, String udid, String systemPort, String appPackage, String appActivity) throws IOException  {
+	@BeforeTest(alwaysRun = true)
+	@Parameters({ "deviceName", "udid", "systemPort", "appPackage", "appActivity" })
+	public void setup(String deviceName, String udid, String systemPort, String appPackage, String appActivity)
+			throws IOException {
 
-    	URL url = new URL("http://127.0.0.1:4444/wd/hub");
+		URL url = new URL("http://127.0.0.1:4444/wd/hub");
 
+		DesiredCapabilities capabilitiesUtil = new DesiredCapabilities();
+		capabilitiesUtil.setCapability("udid", udid);
+		capabilitiesUtil.setCapability("deviceName", deviceName);
+		capabilitiesUtil.setCapability("platformName", "Android");
+		capabilitiesUtil.setCapability("newCommandTimeout", 20000);
+		// capabilitiesUtil.setCapability("platformVersion", "9");
+		// capabilitiesUtil.setCapability("app",
+		// "D:\\appium\\signed_KT_GiGAGenie4_3.3.3(170)_211001-GoogleStore-release.apk");
+		capabilitiesUtil.setCapability("appPackage", appPackage);
+		capabilitiesUtil.setCapability("appActivity", appActivity);
+		capabilitiesUtil.setCapability("systemPort", systemPort);
+		capabilitiesUtil.setCapability("autoGrantPermissions", true);
+		capabilitiesUtil.setCapability("noReset", false);
 
-    	DesiredCapabilities capabilitiesUtil = new DesiredCapabilities();
-    	capabilitiesUtil.setCapability("udid",udid);
-    	capabilitiesUtil.setCapability("deviceName", deviceName);
-    	capabilitiesUtil.setCapability("platformName", "Android");
-    	capabilitiesUtil.setCapability("newCommandTimeout", 20000);
- //    	capabilitiesUtil.setCapability("platformVersion", "9");
- //   	capabilitiesUtil.setCapability("app", "D:\\appium\\signed_KT_GiGAGenie4_3.3.3(170)_211001-GoogleStore-release.apk");
-    	capabilitiesUtil.setCapability("appPackage", appPackage);
-    	capabilitiesUtil.setCapability("appActivity", appActivity);
-    	capabilitiesUtil.setCapability("systemPort", systemPort);
-    	capabilitiesUtil.setCapability("autoGrantPermissions", true);
-    	capabilitiesUtil.setCapability("noReset", false);
+		driver = new AndroidDriver<>(url, capabilitiesUtil);
 
-    	driver = new AndroidDriver<>(url, capabilitiesUtil);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-    	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
 
-    }
+	@AfterTest(alwaysRun = true)
+	public void teardown() {
+		if (driver != null) {
 
-
-
-    @AfterTest(alwaysRun = true)
-    public void teardown(){
-        if(driver !=null) {
-
-        	driver.quit();
-        }
-    }
+			driver.quit();
+		}
+	}
 }
